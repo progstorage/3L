d90284fcc9fb32782254a04a5b8179ae0e87968d
progstorage <prog.storage@yandex.ru>
2020-11-02 20:26:42 +0300

    add source folder; .h file and main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "./source/3L.h"
+
+int main(void) {
+	string path = "tests/in/";
+	Menu ME(path); //Меню выбора тесового файла с координатами точек
+
+	int len = ME.x.size();
+	double d;	//"диаметр" фигуры - максимальное расстояние между точками, 0.04*d - расстояние до точек уровней 
+	vector_pairs sorted_point = sort(ME.x, ME.y, &d);
+
+	vector_pairs l1(len);
+	vector_pairs l3(len);
+	levels(sorted_point, d, l1, l3);
+
+	Matrix M(len * 3, vector<double>(6));
+	fill_3L_Matrix_2nd_power(M, sorted_point, l1, l3);
+
+	solve_system(M, d);
+
+	while (ME.to_draw) {
+		ME.test_file = "python plot_3L.py " + ME.test_file;
+		char plot[50];
+		strcpy_s(plot, ME.test_file.c_str());
+		system(plot);		// отрисовка
+		ME.start();
+	}
+
+	sorted_point.clear();
+
+	return 0;
+}
---
+++ source/3L.h
@@ -0,0 +1,47 @@
+#pragma once
+//#include "stdafx.h"
+#include <stdlib.h>
+#include <cmath> 
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <string>
+#include <vector>
+
+using namespace std;
+namespace fs = experimental::filesystem;
+using vector_pairs = vector<pair<double, double>>;
+using Matrix = vector<vector<double>>;
+
+
+double diff(pair<double, double>, pair<double, double>);
+vector_pairs sort(const vector<double>&, const vector<double>&, double*);
+void levels(vector_pairs, double, vector_pairs&, vector_pairs&);
+void print_Matrix(const Matrix&);
+Matrix transpose_Matrix(const Matrix&);
+Matrix transpose_Matrix(const Matrix&);
+Matrix mult_Matrix(const Matrix&, const Matrix&);
+void fill_3L_Matrix_2nd_power(Matrix&, const vector_pairs&, const vector_pairs&, const vector_pairs&);
+double det_Matrix(const Matrix&);
+Matrix inverse_Matrix(const Matrix&);
+void solve_system(Matrix&, double);
+
+
+class Menu {
+public:
+	string dir;
+	vector<string> files;
+	int n;
+	vector<double> x;
+	vector<double> y;
+	string test_file;
+	bool to_draw;
+
+	Menu(string);
+	void get_files(string);
+	void print_menu();
+	void select_file();
+	void split(string, double*, double*);
+	void read_file();
+	void start();
+};
